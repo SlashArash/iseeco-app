ca0f56bb988ed7062d11deb706a939e4acaf8376
SlashArash <arash.kadkhodaei@gmail.com>
2019-05-06 15:40:59 +0430

    fix changing of lamp status

--- src/store/devices/reducer.ts
+++ src/store/devices/reducer.ts
@@ -25,11 +25,17 @@ const DevicesReducer = (
         const device = draft[action.deviceNumber]['A'];
         const deviceType = mapDeviceType(device.type);
         if (deviceType === 'lamp') {
-          draft[action.deviceNumber]['A'].active =
-            action.part3 === '1' ? 'on' : 'off';
+          if (action.part3 === '1') {
+            draft[action.deviceNumber]['A'].active = 'on';
+          } else if (action.part3 === '0') {
+            draft[action.deviceNumber]['A'].active = 'off';
+          }
           if (draft[action.deviceNumber]['B']) {
-            draft[action.deviceNumber]['B'].active =
-              action.part4 === '1' ? 'on' : 'off';
+            if (action.part4 === '1') {
+              draft[action.deviceNumber]['B'].active = 'on';
+            } else if (action.part4 === '0') {
+              draft[action.deviceNumber]['B'].active = 'off';
+            }
           }
         } else if (deviceType === 'thermostat') {
           draft[action.deviceNumber]['A'].fanSpeed = Number(action.part3);
